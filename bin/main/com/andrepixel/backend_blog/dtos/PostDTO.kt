f8b458b22763b782293c3c9ca1cbf83b265d1845
André <devandrecontact@gmail.com>
2023-10-07 13:10:50 -0300

    fix: update gradle

---
+++ bin/main/com/andrepixel/backend_blog/dtos/PostDTO.kt
@@ -0,0 +1,1 @@
+class PostDTO(val title: String, val tags: Array<String>, val text: Array<String>)
---
+++ src/main/kotlin/com/andrepixel/backend_blog/services/ReturnPostsService.kt
@@ -0,0 +1,6 @@
+import org.springframework.stereotype.Service
+
+@
+class ReturnPostsService() {
+
+}
